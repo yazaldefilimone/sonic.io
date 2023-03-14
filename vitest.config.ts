519997deea6d9b7e9f1e1e5915d1a16b5a6a173b
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-14 20:48:09 +0200

    chore: config vitest

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [tsconfigPaths()],
+});
